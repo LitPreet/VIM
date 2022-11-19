4b8de631e33d01ea8f719afc8caf09d89f2dc78d
Preet bhardwaj <preetb059@gmail.com>
2022-11-19 15:53:34 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const link = document.querySelectorAll('li');
+const navBar = document.querySelector(".navbar")
+const cursor = document.querySelector(".cursor");
+const navBag = document.querySelector(".nav-bag");
+const hammburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-ul")
+// const navItem = document.(".nav-item");
+const navLogo = document.querySelector(".nav-logo");
+const heading = document.querySelector(".h1");
+const heading2 = document.querySelector(".h2");
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+})
+navLogo.addEventListener("mouseenter", (e) => {
+    cursor.classList.add("hover")
+})
+navLogo.addEventListener("mouseleave", (e) => {
+    cursor.classList.remove("hover")
+})
+
+
+navBag.addEventListener("mouseenter", (e) => {
+    cursor.classList.add("hover")
+})
+navBag.addEventListener("mouseleave", (e) => {
+    cursor.classList.remove("hover")
+})
+heading.addEventListener('mouseenter',(e)=>{
+    cursor.classList.add("circle")
+    cursor.classList.add("hover1")
+})
+heading.addEventListener('mouseleave',(e)=>{
+    cursor.classList.remove("circle")
+    cursor.classList.remove("hover1")
+})
+heading2.addEventListener('mouseenter',(e)=>{
+    cursor.classList.add("circle")
+    cursor.classList.add("hover1")
+})
+heading2.addEventListener('mouseleave',(e)=>{
+    cursor.classList.remove("circle")
+    cursor.classList.remove("hover1")
+})
+
+
+
+hammburger.addEventListener("click",()=>{
+    hammburger.classList.toggle("active");
+    navBar.classList.toggle("black")
+    navmenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click"),()=>{
+    hammburger.classList.remove("active");
+    navmenu.classList.remove("active")
+})
