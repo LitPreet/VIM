15e9e734ba2183a52aeac602f633985ad37b873b
Preet bhardwaj <preetb059@gmail.com>
2022-11-30 23:00:41 +0530

    final

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ const hand = document.querySelectorAll(".hand1");
 //     }
 //  });
 
+// Wrap every letter in a span
+var textWrapper = document.querySelectorAll('.ml13');
+textWrapper.forEach(text=>{
+    text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+})
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml13 .letter',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  })
 
 let mm = new MagnetMouse({
     magnet: {
